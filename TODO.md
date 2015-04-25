70f35d4d7f4011a2a77acbcc2d2a574ecf5ef912
sjtindell <sjtindell@gmail.com>
2015-04-25 13:06:58 -0700

    Updated manage.py with quick db management funcs, updated .gitignore to clarify, added items to TODO

---
+++ TODO.md
@@ -0,0 +1,1 @@
+Add dates to mac_to_ssid table and display on widget.
--- src/manage.py
+++ src/manage.py
@@ -1,8 +1,26 @@
+import sys
 import sqlite3
 from sqlwrap import Database
 
 # for my initial testing, quickly build fresh db
 if __name__ == '__main__':
-	with Database('ssids.db') as db:
-		db.create_mac_ssid_table()
-		db.create_ssid_coords_table()	
+	arg = None
+	
+	try:
+		arg = sys.argv[1]
+	except IndexError:
+		pass
+
+	if arg == 'del':
+		table = sys.argv[2]
+		with Database('ssids.db') as db:
+			db.cursor.execute('DELETE FROM {0}'.format(table))
+	elif arg == 'see':
+		table = sys.argv[2]
+		with Database('ssids.db') as db:
+			print db.get_rows('mac_to_ssid', 10)
+	else:
+		with Database('ssids.db') as db:
+			db.create_mac_ssid_table()
+			db.create_ssid_coords_table()
+			db.create_ssid_map_table()
