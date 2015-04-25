e6b38dabf386de4b39b46d762b72b0104e86f463
sjtindell <sjtindell@gmail.com>
2015-04-25 03:49:08 -0700

    Updated .gitignore with html dir and added second table to manage.py create db func

--- src/manage.py
+++ src/manage.py
@@ -1,8 +1,8 @@
 import sqlite3
-from db_api import Database
+from sqlwrap import Database
 
 # for my initial testing, quickly build fresh db
 if __name__ == '__main__':
 	with Database('ssids.db') as db:
 		db.create_mac_ssid_table()
-	
+		db.create_ssid_coords_table()	
