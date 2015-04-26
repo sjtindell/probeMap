9abd4e261cfb1cd84a7d5025ab56efe2e5c34063
sjtindell <sjtindell@gmail.com>
2015-04-26 11:22:41 -0700

    Updated sqlwrap to use parameterized statements rather than formatted strings, added logging to TODO

--- TODO.md
+++ TODO.md
@@ -1,1 +1,2 @@
 Add dates to mac_to_ssid table and display on widget.
+Add logging module.
--- src/sqlwrap.py
+++ src/sqlwrap.py
@@ -48,19 +48,18 @@ class Database(object):
 		self.cursor.execute('''CREATE TABLE ssid_to_map
 			(ssid, html_string)''')
 		
-	def insert_mac_ssid(self, mac, ssid):	
-		self.cursor.execute('''INSERT INTO mac_to_ssid VALUES 
-			({0}, {1})'''.format(mac, ssid))
+	def insert_mac_ssid(self, mac, ssid):
+		cmd = 'INSERT INTO mac_to_ssid VALUES (?, ?)'	
+		self.cursor.execute(cmd, (mac, ssid,))
 
 	def insert_ssid_coords(self, ssid, lat, lon):
-		self.cursor.execute('''INSERT INTO ssid_to_coords VALUES 
-			({0}, {1}, {2})'''.format(ssid, lat, lon))
+		cmd = 'INSERT INTO ssid_to_coords VALUES (?, ?, ?)'
+		self.cursor.execute(cmd, (ssid, lat, lon,))
 
 	def insert_ssid_map(self, ssid, html_string):
-		cmd = "INSERT INTO ssid_to_map VALUES (?, ?)"
+		cmd = 'INSERT INTO ssid_to_map VALUES (?, ?)'
 		self.cursor.execute(cmd, (ssid, html_string,))
 		
-	# getters can be properties later
 	def get_rows(self, table, stop=1, start=1):
 		string = 'SELECT * FROM {0} LIMIT {1}, {2}'
 		cmd = string.format(table, start, stop)
@@ -75,8 +74,7 @@ class Database(object):
 	def get_ssid_map(self, ssid):
 		cmd = 'SELECT html_string FROM ssid_to_map WHERE ssid={0}'.format(repr(ssid))
 		self.cursor.execute(cmd)
-		# [0] for get only one map string if name multiple times
-		html_string_list = [string for sub_tuple in self.cursor.fetchall() for string in sub_tuple][0]
-		return html_string_list
+		html_string = self.cursor.fetchall()[0][0]
+		return html_string
 
 
